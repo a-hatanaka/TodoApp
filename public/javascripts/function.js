ed69211993745a43c2093a692bb46d46b1bd327b
atsushi1197 <atsushi1197@hotmail.com>
2022-04-12 18:29:35 +0900

    display all taks 100%, creating task process 50%

---
+++ public/javascripts/function.js
@@ -0,0 +1,7 @@
+function escapeHTML(s) {
+    return s.replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;" )
+            .replace(/'/g, "&#39;" );
+  }
--- routes/api/index.js
+++ routes/api/index.js
@@ -4,10 +4,11 @@ const items = require("../../src/items");
 var router = express.Router();
 
 /* 商品一覧を取得するルーティング */
-router.get("/items", function (req, res, next) {
-  const itemsList = items.getListItem();
+router.get("/items", async function (req, res, next) {
+  const itemsList = await items.getListItem();
   res.send(itemsList);
 });
+
 
 /*１件の商品情報を取得するルーティング */
 router.get("/items/:id", function (req, res, next) {
